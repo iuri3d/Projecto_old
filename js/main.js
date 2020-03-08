03b25bd70d78cc541c9a19eb93a2eea52dff7132
Iuri Pereira <iuri.3d@gmail.com>
2020-03-08 17:43:29 +0000

    Calendario, ajustes css

--- js/main.js
+++ js/main.js
@@ -3,3 +3,21 @@ $(".hamburger").click(function() {
     $(".contentbox").toggleClass("open");
 
 });
+
+new Chart (document.getElementById('sellers'),{
+    type: 'pie',
+    data: {
+        labels:["João", "Maria", "Sandra", "Francisco"],
+        datasets:[{
+            label: "Vendedores",
+            backgroundColor:["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9"],
+            data: [1234,234,4567,789]
+        }]
+    },
+    options: {
+        title:{
+            display:true,
+            text:'Vendedores'
+        }
+    }
+});
