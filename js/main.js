d6e01808b5b230e934dd2f47698fe72b23922db5
Iuri Pereira <iuri.3d@gmail.com>
2020-03-09 14:48:54 +0000

    login modal

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ $(".hamburger").click(function() {
 
 });
 
-new Chart (document.getElementById('sellers'),{
+/*new Chart (document.getElementById('sellers'),{
     type: 'pie',
     data: {
         labels:["João", "Maria", "Sandra", "Francisco"],
@@ -19,5 +19,34 @@ new Chart (document.getElementById('sellers'),{
             display:true,
             text:'Vendedores'
         }
+    }
+});*/
+
+$(document).ready(function(){
+
+    // Get the modal
+    var modal = document.getElementById("myModal");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("login");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on the button, open the modal
+    btn.onclick = function() {
+        modal.style.display = "flex";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+    modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
     }
 });
