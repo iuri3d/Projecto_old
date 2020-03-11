0854261072f6dc9f09bb1060ee4647a4cf99cebf
Iuri Pereira <iuri.3d@gmail.com>
2020-03-11 15:37:28 +0000

    google chart

--- js/main.js
+++ js/main.js
@@ -1,48 +1,34 @@
-$(".hamburger").click(function() {
-    $(".sidebar").toggleClass("open");
-    $(".contentbox").toggleClass("open");
-
-});
-
-/*new Chart (document.getElementById('sellers'),{
-    type: 'pie',
-    data: {
-        labels:["João", "Maria", "Sandra", "Francisco"],
-        datasets:[{
-            label: "Vendedores",
-            backgroundColor:["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9"],
-            data: [1234,234,4567,789]
-        }]
-    },
-    options: {
-        title:{
-            display:true,
-            text:'Vendedores'
-        }
-    }
-});*/
-
 $(document).ready(function(){
+
+    ////// Toggle side bar open //////
+    $(".hamburger").click(function() {
+        $(".sidebar").toggleClass("open");
+        $(".contentbox").toggleClass("open");
+    
+    });
 
     // Get the modal
     var modal = document.getElementById("myModal");
 
     // Get the button that opens the modal
-    var btn = document.getElementById("login");
+    //var btn = document.getElementById("login");
 
     // Get the <span> element that closes the modal
     var span = document.getElementsByClassName("close")[0];
 
     // When the user clicks on the button, open the modal
-    btn.onclick = function() {
-        modal.style.display = "flex";
-    }
+    $('#login').on('click',function(){
+        $('#myModal').css('display',"flex");
+    });
+
 
     // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
+    $('.close').on('click', function(){
+        $('#myModal').css('display','none');
+    });
+    /*span.onclick = function() {
     modal.style.display = "none";
-        backdrop
-    }
+    }*/
 
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function(event) {
@@ -50,4 +36,40 @@ $(document).ready(function(){
         modal.style.display = "none";
     }
     }
+
+          // Load the Visualization API and the corechart package.
+          google.charts.load('current', {'packages':['corechart']});
+
+          // Set a callback to run when the Google Visualization API is loaded.
+          google.charts.setOnLoadCallback(drawChart);
+    
+          // Callback that creates and populates a data table,
+          // instantiates the pie chart, passes in the data and
+          // draws it.
+          function drawChart() {
+    
+            // Create the data table.
+            var data = new google.visualization.DataTable();
+            data.addColumn('string', 'Topping');
+            data.addColumn('number', 'Slices');
+            data.addRows([
+              ['Mushrooms', 3],
+              ['Onions', 1],
+              ['Olives', 1],
+              ['Zucchini', 1],
+              ['Pepperoni', 2]
+            ]);
+    
+            // Set chart options
+            var options = {'title':'How Much Pizza I Ate Last Night'};
+    
+            // Instantiate and draw our chart, passing in some options.
+            var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+            chart.draw(data, options);
+          }
+
+          $(window).resize(function(){
+            drawChart();
+          });
+    
 });
